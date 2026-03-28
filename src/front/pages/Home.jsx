8078891edb055f9904b31ff238e31a4b75811077
jhonyes04 <jhonyes@hotmail.com>
2026-03-28 08:44:19 +0100

    Proyecto acabado?

--- src/front/pages/Home.jsx
+++ src/front/pages/Home.jsx
@@ -1,19 +1,39 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { tokenValido } from '../services/validarToken'
+import { useState, useEffect } from "react";
 
 export const Home = () => {
+	const [logueado, setLogueado] = useState(false)
+	const location = useLocation()
+	const navigate = useNavigate()
+
+	useEffect(() => {
+		setLogueado(tokenValido())
+	}, [location])
+
+	const handleClickLogout = () => {
+		sessionStorage.removeItem('token')
+		sessionStorage.removeItem('token_expires_at')
+
+		navigate('/')
+	}
 	return (
 		<div className="container mt-5">
 			<div className="card col-8 mx-auto">
 				<div className="card-body text-center">
 					<h1>Bienvenid@</h1>
-					<div className="container d-flex">
-						<div className="col-6">
-							<Link to={'/login'} className="btn btn-dark">Inicia sesión</Link>
+					{logueado ? (
+						<button onClick={handleClickLogout} className="btn btn-danger">Cerrar sesión</button>
+					) : (
+						<div className="container d-flex">
+							<div className="col-6">
+								<Link to={'/login'} className="btn btn-dark">Inicia sesión</Link>
+							</div>
+							<div className="col-6">
+								<Link to={'/register'} className="btn btn-primary">Regístrate</Link>
+							</div>
 						</div>
-						<div className="col-6">
-							<Link to={'/register'} className="btn btn-primary">Regístrate</Link>
-						</div>
-					</div>
+					)}
 				</div>
 			</div>
 		</div>
